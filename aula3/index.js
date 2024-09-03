c54061c77934659019c101f17107bf6fedd02f5d
Samuel Silveira <samuel.silveira@live.com>
2024-09-02 23:15:12 -0300

    Commit das aulas 3, 4 e 5

---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Samuel "Silveira"'); // Permite utilizar aspas duplas no texto
+console.log("Samuel 'Silveira'"); // Permite utilizar aspas simples no texto
+console.log(`'Samuel' "Silveira"`); // Permite utilizar aspas simples e duplas no texto
+
+console.log(35, 15.85, 'Samuel Silveira de Novo'); // Inteiros e ponto flutuante são considerados 'number'
