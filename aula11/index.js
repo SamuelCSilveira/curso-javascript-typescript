380fb2feaf44282584c2cfb1d04abcad5590a26b
Samuel Silveira <samuel.silveira@live.com>
2024-09-04 23:57:46 -0300

    Commit das aulas 9, 10 e 11

---
+++ aula11/index.js
@@ -0,0 +1,33 @@
+/**
+ * Aritméticos 
+ * + Adição / Concatenação
+ * - Subtração
+ * / Divisão
+ * * Multiplicação
+ * ** Potenciação
+ * % Resto da divisão
+ * 
+ * Precedência: 
+ * 1º () 
+ * 2º ** 
+ * 3º * / % 
+ * 4º + -
+ * 
+ * Incremento = ++
+ * Decremento = --
+ * 
+ * Operadores de atribuição
+ * +=
+ * -=
+ * *=
+ * /=
+ * **=
+ * 
+ * NaN - Not a number
+ * parseInt (inteiro)
+ * parseFloat (decimais)
+ */
+const num1 = 10;
+const num2 = parseFloat('5.2');
+console.log(num1 + num2);
+console.log(typeof num2);
---
+++ aula9/index.js
@@ -0,0 +1,4 @@
+var nome = 'Samuel'; 
+var nome = 'Christian';
+
+console.log(nome);
