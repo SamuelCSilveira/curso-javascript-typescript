8dce47fb604207ac290d1f107273218aea7cd983
Samuel Silveira <samuel.silveira@live.com>
2024-09-16 23:26:29 -0300

    Commit das aulas 22, 23 e 24

---
+++ aula23/index.js
@@ -0,0 +1,18 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && false && true;
+// const expressaoOr = false || false || false || false;
+// console.log(expressaoOr);
+
+// const usuario = 'Samuel'; // Form usuário digitou
+// const senha = '123456'; // Form usuário digitou
+
+//                       True                  False
+// const vaiLogar = usuario === 'Samuel' && senha ==='12345';
+// console.log(vaiLogar);
+
+console.log(!true);
