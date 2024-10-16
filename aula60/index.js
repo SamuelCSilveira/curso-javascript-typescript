aa1cd74962c368ba9d46df83193ff65899773152
Samuel Silveira <samuel.silveira@live.com>
2024-10-15 22:49:05 -0300

    Commit das aulas 59 e 60

---
+++ aula60/index.js
@@ -0,0 +1,11 @@
+// // Valor por referência
+// //                 0         1        2         3          4
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const novo = nomes.slice(1, -1);
+// console.log(novo);
+
+// const nome = 'Samuel, Christian, da Silveira';
+// const nomes = nome.split(' ');
+const nomes = ['Samuel', 'Christian', 'da Silveira'];
+const nome = nomes.join(' ');
+console.log(nome);
