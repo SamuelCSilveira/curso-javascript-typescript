3a7a3696e1213c40f7d72d113099965764b6fd5c
Samuel Silveira <samuel.silveira@live.com>
2024-10-18 00:34:37 -0300

    Commit das aulas 61, 62, 63 e 64

---
+++ aula61/index.js
@@ -0,0 +1,19 @@
+//               -5       -4       -3         -2         -1
+//                0        1        2          3          4
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// push
+// nomes.splice(nomes.length, 0, 'Samuel');
+// console.log(nomes)
+
+// unshift
+// nomes.splice(0, 0, 'Samuel', 'Silveira');
+// console.log(nomes);
+
+// nomes.splice(indice, delete, elem1, elem2, elem3);
+// pop
+// const removidos = nomes.splice(-1, 1);
+
+// shift
+// const removidos = nomes.splice(0, 1);
+// console.log(nomes, removidos);
